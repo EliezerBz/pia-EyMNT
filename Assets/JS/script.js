f29963682ff6d6d7b9008d2d46a5fdfa10f53a49
Luis Mtz <luisalonso20101@hotmail.com>
2024-10-18 00:29:21 -0600

    Modificacion rutas, y carga de archivos js

---
+++ Assets/JS/script.js
@@ -0,0 +1,15 @@
+const btnlogin= document.getElementById("log"),
+      btnregister = document.getElementById("reg"),
+      containerFormRegister = document.querySelector(".register"),
+      containerFormLogin = document.querySelector(".login");
+
+btnlogin.addEventListener("click", e => {
+    containerFormRegister.classList.add("hide");
+    containerFormLogin.classList.remove("hide")
+})
+
+
+btnregister.addEventListener("click", e => {
+    containerFormLogin.classList.add("hide");
+    containerFormRegister.classList.remove("hide")
+})
